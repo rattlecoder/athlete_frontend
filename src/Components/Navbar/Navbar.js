02e1a764fedb6587a93d3b806abbd9b5c28e8422
rattlecoder <shashank.trivedi02@gmail.com>
2023-12-10 14:12:43 +0530

    navbar design fix

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, Fragment } from 'react';
 import useStyles from './styles';
-import { Container,AppBar,Typography,Grow,Grid, Toolbar, Avatar, Button } from '@material-ui/core';
+import { Container,AppBar,Typography,Grow,Grid, Toolbar, Avatar, Button, Box } from '@material-ui/core';
 import img from '../../Images/mig2.webp';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
@@ -41,27 +41,29 @@ const Navbar = () => {
     }
 
   return (
-       <AppBar className={classes.appBar} position='static' color='inherit'>
-          <div className={classes.brandContainer}>
-            <Typography component={Link} to="/" className={classes.heading} variant='h2' align='center'>Memories</Typography>
-            <img className={classes.image} src={img} alt='memories' height='90' />
-          </div>
-          <Toolbar className={classes.toolbar}>
-             { user?.result ? (
-                <div className={classes.profile}>
-                  <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
-                  <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
-                  <Button variant='contained' className={classes.logout} onClick={logout} color='secondary'>Logout</Button>
-                </div>
-             ) : (
-               
-                <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
-                
-             )
-
-             }
-          </Toolbar>
-       </AppBar>
+       
+    <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Grid container spacing={2} rowSpacing={10} justifyContent='space-between' style={{ width: '100%' }}>
+          <Grid item xs={12} sm={6}>
+          <Typography component={Link} to="/" className={classes.heading} variant='h2' align='center'>ATLETAS</Typography>
+          </Grid>
+          { user?.result && (
+             <Grid item xs={6} sm={2}>
+               <Avatar style={{ marginLeft: '25px', marginTop: '10px' }} className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+               <Typography className={classes.userName} variant='h6'>{user.result.name}</Typography>
+             </Grid>
+          )}
+          { user?.result ? (
+            <Grid item xs={6} sm={2}>
+               <Button variant='contained' style={{ marginLeft: '25px', height: '50px', marginTop: '10px' }} className={classes.logout} onClick={logout} color='secondary'>Logout</Button>
+            </Grid>
+          ) : (
+            <Grid container item xs={9} sm={2} style={{ direction:'row', justify:'center'}}>
+               <Button style={{ marginLeft: '25px', height: '50px', marginTop: '10px' }} component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+            </Grid>
+          )}
+        </Grid>
+      </AppBar>
   )
 }
 
